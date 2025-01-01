4a000cb94a4b8a56b570aa4edacf8592ffbf7d4d
bryonbaker <bryonbakeraus@gmail.com>
2025-01-02 10:34:10 +1100

    Added makefile

---
+++ Makefile
@@ -0,0 +1,20 @@
+# Makefile for building shakeout-app container
+
+GO_VERSION := 1.23.4
+GO_FILE := go$(GO_VERSION).linux-amd64.tar.gz
+GOLANG_URL := https://go.dev/dl/$(GO_FILE)
+IMAGE_NAME := quay.io/bryonbaker/go-builder
+TAG := $(GO_VERSION)
+
+.PHONY: all build push clean
+
+all: build push
+
+build:
+	buildah unshare ./mk-builder.sh $(IMAGE_NAME):$(TAG)
+
+push:
+	podman push $(IMAGE_NAME):$(TAG)
+
+clean:
+	podman rmi -f $(IMAGE_NAME):$(TAG) || true
--- mk-builder.sh
+++ mk-builder.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 #
-# Run this shell script after you have run the command: "buildah unshare"
+# Shell script to build a Golang builder image using Buildah.
+# This script should be run within a buildah unshare session.
 #
-echo "Building golang buildah image."
-echo "Run buildah unshare before you run this script"
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Function to print usage and exit
+usage() {
+    echo "Usage: buildah unshare ./mk-builder.sh <image name:tag>"
+    echo "Example: buildah unshare ./mk-builder.sh quay.io/username/go-builder:1.23.4"
+    exit 1
+}
+
+# Check if an image name is provided
+if [ -z "$1" ]; then
+    usage
+fi
+
+IMAGE_NAME="$1"
+
+echo "Building golang buildah image: $IMAGE_NAME"
 
 export GO_VERSION=1.23.4
 export GO_FILE=go${GO_VERSION}.linux-amd64.tar.gz
@@ -12,6 +30,10 @@ echo $GOLANG_URL
 
 container=$(buildah from registry.access.redhat.com/ubi8/ubi-minimal:latest)
 mnt=$(buildah mount $container)
+
+# Ensure /usr/local exists
+mkdir -p $mnt/usr/local
+
 # Download and install golang
 wget $GOLANG_URL -O $mnt/tmp/$GO_FILE && \
 rm -rf $mnt/usr/local/go && \
@@ -20,4 +42,10 @@ rm $mnt/tmp/$GO_FILE
 
 echo "PATH=\$PATH:/usr/local/go/bin" >> $mnt/root/.bashrc
 
-buildah commit --format docker $container go-builder:$GO_VERSION
+# Commit the container to create the image with the provided image name
+buildah commit --format docker "$container" "$IMAGE_NAME"
+
+# Unmount the container
+buildah unmount "$container"
+
+echo "Golang builder image '$IMAGE_NAME' created successfully."
